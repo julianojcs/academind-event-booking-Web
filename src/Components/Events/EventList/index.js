ffd123b3dde64018d4de21c46ad9ff54298caaa2
Juliano Costa Silva <apfjuliano@gmail.com>
2021-04-07 13:57:16 -0300

    Bug Fixing

--- src/Components/Events/EventList/index.js
+++ src/Components/Events/EventList/index.js
@@ -1,15 +1,7 @@
-import { useContext } from 'react'
-import { UserContext } from '../../../UserContext'
 import styled from 'styled-components'
 import EventItem from './EventItem'
 
 function EventList({ events, authUserId, onViewDetail }) {
-  const context = useContext(UserContext)
-  const userId = context.data ? context.data.login._id : null
-
-  const onViewDetaila = (eventId) => {
-    console.log(eventId);
-  }
   return (
     <EventsList>
       {events.map((event) => {
